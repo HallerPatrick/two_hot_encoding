02860a67ab3bdd885da90346b06d7a80dec95d46
Patrick Haller <patrickhaller40@googlemail.com>
2022-02-18 16:50:54 +0100

    Fix soft labeling for ngrams > 2

--- two_hot_encoding.py
+++ two_hot_encoding.py
@@ -44,8 +44,7 @@ def n_hot(t, num_clases):
 
 @lru_cache(maxsize=5)
 def soft_dist(n):
-    # return [0.7, 0.3]
-    return [0.5, 0.5]
+    return [1 / n] * n
 
 
 def soft_n_hot(input, num_classes):
