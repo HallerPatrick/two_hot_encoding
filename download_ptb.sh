4acf21d4e2db7f910667db5d595cdae189925787
Patrick Haller <patrickhaller40@googlemail.com>
2022-02-23 19:48:17 +0100

    Using normal PTB dataset in download

--- download_ptb.sh
+++ download_ptb.sh
@@ -2,9 +2,9 @@ echo "- Downloading Penn Treebank (PTB)"
 wget --quiet --continue http://www.fit.vutbr.cz/~imikolov/rnnlm/simple-examples.tgz
 tar -xzf simple-examples.tgz
 
-echo "- Downloading Penn Treebank (Character)"
 mkdir -p data/pennchar
 
-mv simple-examples/data/ptb.char.train.txt data/pennchar/train.txt
-mv simple-examples/data/ptb.char.test.txt data/pennchar/test.txt
-mv simple-examples/data/ptb.char.valid.txt data/pennchar/valid.txt
+echo "Saved in data/pennchar"
+mv simple-examples/data/ptb.train.txt data/pennchar/train.txt
+mv simple-examples/data/ptb.test.txt data/pennchar/test.txt
+mv simple-examples/data/ptb.valid.txt data/pennchar/valid.txt
