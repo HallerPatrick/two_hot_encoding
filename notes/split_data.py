a5f4f4b2c1f09ab0d5fd9d389d1817c79795e840
Patrick Haller <patrickhaller40@googlemail.com>
2022-03-14 16:11:09 +0100

    Accepts paths for splitting datat

--- notes/split_data.py
+++ notes/split_data.py
@@ -1,15 +1,26 @@
+import os
+from pathlib import Path
 
 def split_data(path, target_path):
-    
+
     train_split = 0.75
     test_split = 0.15
     valid_split = 0.10
 
-    with open(path) as f:
-        lines = f.readlines()
+    
+    if Path(path).is_dir():
+        lines = []
+        for file in os.listdir(path):
+            file_path = os.path.join(path, file)
+
+            with open(file_path, "r") as f:
+                lines.extend(f.readlines())
+    else:
+        with open(path) as f:
+            lines = f.readlines()
 
     total_lines = len(lines) * 0.1
-    
+
     train_slice = total_lines * train_split
     test_slice = train_slice + (test_split * total_lines)
     valid_slice = test_slice + (valid_split * total_lines)
@@ -34,4 +45,3 @@ if __name__ == "__main__":
     assert len(sys.argv) == 3
 
     split_data(sys.argv[1], sys.argv[2])
-
