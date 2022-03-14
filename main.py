c16defde32d7fc314cd4f2eb25d835ccfe8b2a2c
Patrick Haller <patrickhaller40@googlemail.com>
2022-03-14 15:13:34 +0100

    FOrmatting, remove data parallel from main branch

--- main.py
+++ main.py
@@ -10,7 +10,13 @@ import model as _model
 
 from args import argparser_train
 from loss import CrossEntropyLossSoft
-from torch_utils import batchify, count_parameters, export_onnx, get_batch, repackage_hidden
+from torch_utils import (
+    batchify,
+    count_parameters,
+    export_onnx,
+    get_batch,
+    repackage_hidden,
+)
 from two_hot_encoding import soft_n_hot
 
 
@@ -32,7 +38,7 @@ def run_train(args):
     # Load data
     ###############################################################################
 
-    corpus = data.Corpus(args.data, device, args.ngrams, args.unk_t)
+    corpus = data.Corpus(args.data, device, args.ngrams, args.unk_t, args.max_dict_size)
 
     print(f"Dictionary Size: {len(corpus.dictionary)}")
 
@@ -44,8 +50,15 @@ def run_train(args):
     ###############################################################################
     # Build the model
     ###############################################################################
-    if args.model == 'Transformer':
-        model = _model.TransformerModel(corpus.dictionary, args.emsize, args.nhead, args.nhid, args.nlayers, args.dropout).to(device)
+    if args.model == "Transformer":
+        model = _model.TransformerModel(
+            corpus.dictionary,
+            args.emsize,
+            args.nhead,
+            args.nhid,
+            args.nlayers,
+            args.dropout,
+        ).to(device)
     else:
         model = _model.RNNModel(
             corpus.dictionary,
@@ -58,22 +71,24 @@ def run_train(args):
             dropout=args.dropout,
         )
 
-    model = torch.nn.DataParallel(model)
-
     count_parameters(model)
-   
+
     # TODO: Weighted loss labels?
-    weights = None # torch.ones((ntokens))
+    weights = None  # torch.ones((ntokens))
     # for n, n_idxs in corpus.ngram_indexes.items():
     #     for idxs in n_idxs:
     #         weights[idxs] = n
-    
+
     criterion = CrossEntropyLossSoft(weight=weights)
-    
+
     if args.optimizer == "adam":
-        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+        optimizer = torch.optim.Adam(
+            model.parameters(), lr=args.lr, weight_decay=args.wdecay
+        )
     elif args.optimizer == "sgd":
-        optimizer = torch.optim.SGD(model.parameters(), lr=args.lr, weight_decay=args.wdecay)
+        optimizer = torch.optim.SGD(
+            model.parameters(), lr=args.lr, weight_decay=args.wdecay
+        )
 
     ###############################################################################
     # Training code
@@ -83,16 +98,16 @@ def run_train(args):
         model.eval()
         total_loss = 0.0
         ntokens = len(corpus.dictionary)
-        
+
         if args.model != "Transformer":
-            hidden = model.module.init_hidden(eval_batch_size)
+            hidden = model.init_hidden(eval_batch_size)
 
         with torch.no_grad():
             for i in range(0, data_source.size(1) - args.ngrams, args.bptt):
                 data, targets = get_batch(data_source, i, args.bptt)
                 targets = soft_n_hot(targets, ntokens)
 
-                if args.model == 'Transformer':
+                if args.model == "Transformer":
                     output = model(data)
                     output = output.view(-1, ntokens)
 
@@ -105,11 +120,15 @@ def run_train(args):
                 # Perplexity only based on unigram candidates
                 if args.unigram_ppl:
                     output = torch.index_select(
-                        output, 1, torch.tensor(corpus.ngram_indexes[1]).to(output.device)
+                        output,
+                        1,
+                        torch.tensor(corpus.ngram_indexes[1]).to(output.device),
                     )
 
                     targets = torch.index_select(
-                        targets, 1, torch.tensor(corpus.ngram_indexes[1]).to(targets.device)
+                        targets,
+                        1,
+                        torch.tensor(corpus.ngram_indexes[1]).to(targets.device),
                     )
                     total_loss += len(data[0]) * criterion(output, targets).item()
                 else:
@@ -124,7 +143,7 @@ def run_train(args):
         start_time = time.time()
         ntokens = len(corpus.dictionary)
         if args.model != "Transformer":
-            hidden = model.module.init_hidden(args.batch_size)
+            hidden = model.init_hidden(args.batch_size)
         for batch, i in enumerate(
             range(0, train_data.size(1) - args.ngrams, args.bptt)
         ):
@@ -151,7 +170,7 @@ def run_train(args):
 
             for p in model.parameters():
                 p.data.add_(p.grad, alpha=-optimizer.param_groups[0]["lr"])
-            
+
             optimizer.step()
             total_loss += loss.item()
 
@@ -201,9 +220,9 @@ def run_train(args):
             # Save the model if the validation loss is the best we've seen so far.
             if not best_val_loss or val_loss < best_val_loss:
                 with open(args.save, "wb") as f:
-                    torch.save(model.module, f)
+                    torch.save(model, f)
 
-                model.module.save("flair_" + args.save)
+                model.save("flair_" + args.save)
 
                 best_val_loss = val_loss
             else:
@@ -222,8 +241,6 @@ def run_train(args):
         # Currently, only rnn model supports flatten_parameters function.
         if args.model in ["RNN_TANH", "RNN_RELU", "LSTM", "GRU"]:
             model.rnn.flatten_parameters()
-
-        model = torch.nn.DataParallel(model)
 
     test_loss = evaluate(test_data)
 
